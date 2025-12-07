c05a9d759b67b8f2da566b5ee07e2d37b2e02ad8
AAPlakhotin <AAPlakhotin@alfabank.ru>
2025-12-08 01:03:23 +0700

    fix import

--- src/tests.py
+++ src/tests.py
@@ -2,7 +2,7 @@ import unittest
 # src/tests.py
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 
 import app as tested_app
 import json
